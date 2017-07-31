121c833887094daa07a2ec7e45dc4cf6823d18e8
Jalen Jackson <jalenjack201@hotmail.com>
2017-07-30 22:58:21 -0400

    new commit

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -10,7 +10,7 @@ export default class MainNav extends React.Component {
                 <div className="nav-container">
                     <NavLink to="/"> <img className="logo-img" src="http://logok.org/wp-content/uploads/2017/01/Juventus-logo-2017-350x302.png"/></NavLink>
                   <div className="inner-nav">
-                  <NavLink activeClassName="about-link" to="/about">About Me</NavLink>
+                  <NavLink className="about-me" activeClassName="about-link" to="/about">About Me</NavLink>
                   </div>
                     <a href="mailto:jalenjack201@hotmail.com?Subject=Web%20Inquiry" target="_top"><img className="email-logo" src="https://cdn0.iconfinder.com/data/icons/basic-thin-ios/512/email-512.png"/></a>
 
